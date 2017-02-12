83fd5cf59ed1b8973c34e5f61b205b6f97135e65
anweshknayak <anweshknayak@gmail.com>
2017-02-12 14:20:52 +0530

    Top stories controller and service added

---
+++ js/app/controllers/TopStoriesController.js
@@ -0,0 +1,29 @@
+// js/app/controllers/TopStoriesController
+(function() {
+'use strict';
+
+  angular
+    .module('app')
+    .controller('TopStoriesController', TopStoriesController);
+
+  TopStoriesController.$inject = ['TopStoriesService'];
+  function TopStoriesController(TopStoriesService) {
+    var vm = this;
+    vm.stories = [];
+    
+
+    activate();
+
+    ////////////////
+
+    function activate() {
+      TopStoriesService 
+        .getStories()
+        .then(function(res) {
+          vm.stories = res.data;
+        });
+
+    }
+  }
+
+})();
